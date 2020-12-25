f7222a933acd9b66534e4d043127e7b6a477ca45
Geogear <mustafaenesbilgic@hotmail.com>
2020-12-26 00:56:04 +0300

    Swagger basics added.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,23 @@
 import express, { Application } from 'express';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
 require('./db/mongoose');
 import landlordRouter from './routes/landlord';
-
 const app: Application = express();
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'Fiyaka API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['app.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
 app.use(express.json());
 app.use(landlordRouter);
 
