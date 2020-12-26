c2fb6ba9cfdec806eab54b0b514f2087df3bc4a4
Geogear <mustafaenesbilgic@hotmail.com>
2020-12-26 13:52:58 +0300

    Landlord model updated.

--- src/models/landlord.ts
+++ src/models/landlord.ts
@@ -9,6 +9,12 @@ export interface ILandlord extends IUser {
 const landlordSchema: mongoose.Schema = new mongoose.Schema();
 landlordSchema.add(userSchema).add({ nick: 'string' });
 
+landlordSchema.virtual('houses', {
+  ref: 'House',
+  localField: '_id',
+  foreignField: '_owner',
+});
+
 const Landlord = mongoose.model<ILandlord>('Landlord', landlordSchema);
 
 export default Landlord;
