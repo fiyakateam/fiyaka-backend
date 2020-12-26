19b45776bda06caa43d7254c62772efbcbbcf592
kyoshi <akusgulu309@gmail.com>
2020-12-26 17:54:05 +0300

    Add login and logout routes to landlord router

--- src/routes/landlord.ts
+++ src/routes/landlord.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { Request } from 'express';
 import Landlord from '../models/landlord';
-const router: express.Router = express.Router();
+import { auth } from '../middleware/auth';
+const router = express.Router();
 
 router.post(
   '/landlords',
@@ -12,6 +13,41 @@ router.post(
       res.status(201).send({ landlord, token });
     } catch (e: any) {
       res.status(400).send(e);
+    }
+  }
+);
+
+router.post(
+  '/landlords/login',
+  async (req: express.Request, res: express.Response) => {
+    try {
+      const user = await Landlord.findByCredentials(
+        req.body.email,
+        req.body.password
+      );
+      const token = await user.generateAuthToken();
+      res.status(200).send({ user, token });
+    } catch (e: any) {
+      res.status(400).send();
+    }
+  }
+);
+
+router.post(
+  '/landlords/logout',
+  auth,
+  async (req: Request, res: express.Response) => {
+    try {
+      console.log(req.headers.token);
+      req.body.user.tokens = req.body.user.tokens.filter((token: any) => {
+        return token.token !== req.headers?.token;
+      });
+      console.log(req.body.user);
+      await req.body.user.save();
+
+      res.send('helo');
+    } catch (e) {
+      res.status(500).send();
     }
   }
 );
