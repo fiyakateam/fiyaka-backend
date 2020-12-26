5b80de430d81fcc8878e496154dc0db00735e920
Geogear <mustafaenesbilgic@hotmail.com>
2020-12-26 18:02:11 +0300

    House route updated.

--- src/routes/house.ts
+++ src/routes/house.ts
@@ -1,21 +1,45 @@
 import express from 'express';
 import House from '../models/house';
 import auth from '../middleware/auth';
-import { getAllJSDocTagsOfKind } from 'typescript';
+import { Mongoose } from 'mongoose';
 const houseRouter: express.Router = express.Router();
 
 // TODO add auth for all of them
-
+/**
+ * @swagger
+ * /houses:
+ *    post:
+ *      description: Create a house
+ *      parameters:
+ *        - name: name
+ *          description: Name of the house
+ *          in: formData
+ *          required: true
+ *          type: string
+ *        - name: address
+ *          description: Address of the house
+ *          in: formData
+ *          required: true
+ *          type: string
+ *      responses:
+ *        201:
+ *          description: Created
+ *        400:
+ *          description: Bad Request
+ */
 houseRouter.post(
   '/houses',
   auth,
   async (req: express.Request, res: express.Response) => {
-    const house = new House(req.body);
+    const house = new House({
+      ...req.body,
+      owner: req.user._id,
+    });
     //TODO add landlord reference with using auth middleware
     try {
       await house.save();
       res.status(201).send({ house });
-    } catch (e: any) {
+    } catch (e) {
       res.status(400).send(e);
     }
   }
@@ -24,6 +48,17 @@ houseRouter.post(
 // GET /houses?limit=n&skip=y //for n pages at each request
 //skip=y to skip y number of requests
 //GET /houses?sortBy=createdAt_asc/_desc //to specify the sorting criteria
+/**
+ * @swagger
+ * /houses:
+ *    get:
+ *      description: Get all houses
+ *      responses:
+ *        200:
+ *          description: Success
+ *        500:
+ *          description: Internal Error
+ */
 houseRouter.get(
   '/houses',
   auth,
@@ -38,12 +73,24 @@ houseRouter.get(
 
     //TODO populate
     try {
-    } catch (e: any) {
+    } catch (e) {
       res.status(500).send(e);
     }
   }
 );
-
+/**
+ * @swagger
+ * /houses/{id}:
+ *    get:
+ *      description: Get a house by id
+ *      responses:
+ *        200:
+ *          description: Success
+ *        404:
+ *          description: House not found
+ *        500:
+ *          description: Internal Error
+ */
 houseRouter.get(
   '/houses/:id',
   auth,
@@ -65,12 +112,38 @@ houseRouter.get(
         return res.status(404).send();
       }
       res.send(house);
-    } catch (e: any) {
+    } catch (e) {
       res.status(500).send(e);
     }
   }
 );
 
+/**
+ * @swagger
+ * /houses/{id}:
+ *    patch:
+ *      description: Update a house by id
+ *      parameters:
+ *        - name: name
+ *          description: Updated name
+ *          in: formData
+ *          type: string
+ *        - name: address
+ *          description: Updated address
+ *          in: formData
+ *          type: string
+ *      responses:
+ *        200:
+ *          description: Success
+ *        400:
+ *          description: Bad Request
+ *        403:
+ *          description: Forbidden
+ *        404:
+ *          description: House not found
+ *        500:
+ *          description: Internal Error
+ */
 houseRouter.patch(
   '/houses/:id',
   auth,
@@ -110,25 +183,37 @@ houseRouter.patch(
   }
 );
 
+/**
+ * @swagger
+ * /houses/{id}:
+ *    delete:
+ *      description: Delete a house by id
+ *      responses:
+ *        200:
+ *          description: Success
+ *        400:
+ *          description: Bad Request
+ *        403:
+ *          description: Forbidden
+ *        404:
+ *          description: House not found
+ *        500:
+ *          description: Internal Error
+ */
 houseRouter.delete(
   'houses/:id',
   auth,
   async (req: express.Request, res: express.Response) => {
-    try {
-      let house: any;
-      const isLandlord = req.user.isLandlord;
+    const isLandlord = req.user.isLandlord;
+    if (!isLandlord) {
+      return res.status(403).send();
+    }
 
-      if (isLandlord) {
-        house = await House.findOneAndDelete({
-          _id: req.params.id,
-          _owner: req.user._id,
-        });
-      } else {
-        house = await House.findOneAndDelete({
-          _id: req.params.id,
-          _occupant: req.user._id,
-        });
-      }
+    try {
+      const house = await House.findOneAndDelete({
+        _id: req.params.id,
+        _owner: req.user._id,
+      });
 
       if (!house) {
         return res.status(404).send();
