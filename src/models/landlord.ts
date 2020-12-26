e136e2118dcf4d13919e3e798033ad5a4006f3ab
kyoshi <akusgulu309@gmail.com>
2020-12-26 17:53:24 +0300

    Add findByCredentials statics to landlordSchema

--- src/models/landlord.ts
+++ src/models/landlord.ts
@@ -1,14 +1,42 @@
-import mongoose from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { userSchema, IUser } from './user';
+import bcrypt from 'bcryptjs';
 
 export interface ILandlord extends IUser {
   nick: string;
+  findByCredentials(email: string, password: string): Promise<ILandlord>;
   // TODO add housees ref somehow ?
+}
+
+export interface ILandlordModel extends Model<ILandlord> {
+  findByCredentials(email: string, password: string): Promise<ILandlord>;
 }
 
 const landlordSchema: mongoose.Schema = new mongoose.Schema();
 landlordSchema.add(userSchema).add({ nick: 'string' });
 
-const Landlord = mongoose.model<ILandlord>('Landlord', landlordSchema);
+landlordSchema.statics.findByCredentials = async (
+  email: string,
+  password: string
+) => {
+  const user = await Landlord.findOne({ email });
+
+  if (!user) {
+    throw new Error('Unable to login');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  }
+
+  return user;
+};
+
+const Landlord = mongoose.model<ILandlord, ILandlordModel>(
+  'Landlord',
+  landlordSchema
+);
 
 export default Landlord;
