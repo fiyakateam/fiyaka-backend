f236b9d1182a5edda8d17c6fa1282a944830c0fb
kyoshi <akusgulu309@gmail.com>
2020-12-27 20:47:43 +0300

    Add cors to the app

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,15 @@
 import express, { Application } from 'express';
-import swaggerJsDoc from 'swagger-jsdoc';
 import swaggerUI from 'swagger-ui-express';
 require('./db/mongoose');
 import landlordRouter from './routes/landlord';
 import houseRouter from './routes/house';
 import authRouter from './routes/auth';
+import * as swaggerDocs from './swagger.json';
+import cors from 'cors';
 const app: Application = express();
 
-const swaggerOptions = {
-  swaggerDefinition: {
-    info: {
-      title: 'Fiyaka API',
-      version: '1.0.0',
-    },
-  },
-  apis: ['./dist/routes/*.js'],
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+app.use(cors());
 
 app.use(express.json());
 
