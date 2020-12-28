76c4f5e3b66056999da72fb5bfa5bb18de8ed536
kyoshi <akusgulu309@gmail.com>
2020-12-28 13:21:19 +0300

    Fix merge errors and change landlords/email from get to post

--- src/routes/landlord.ts
+++ src/routes/landlord.ts
@@ -1,41 +1,9 @@
 import express from 'express';
-import Landlord from '../models/landlord';
 import { auth } from '../middleware/auth';
 import sendCreationEmail from '../services/email';
-import { generateAuthToken } from '../services/auth';
 const router = express.Router();
 
 router.post(
-  '/landlords',
-  async (req: express.Request, res: express.Response) => {
-    try {
-      const landlord = new Landlord(req.body);
-      await landlord.save();
-      const token = generateAuthToken(landlord._id, landlord.role);
-      res.status(201).send({ landlord, token });
-    } catch (e) {
-      res.status(400).send(e);
-    }
-  }
-);
-
-router.post(
-  '/landlords/login',
-  async (req: express.Request, res: express.Response) => {
-    try {
-      const user = await Landlord.findByCredentials(
-        req.body.email,
-        req.body.password
-      );
-      const token = generateAuthToken(user._id, user.role);
-      res.status(200).send({ user, token });
-    } catch (e) {
-      res.status(400).send();
-    }
-  }
-);
-
-router.get(
   '/landlords/email',
   auth,
   (req: express.Request, res: express.Response) => {
