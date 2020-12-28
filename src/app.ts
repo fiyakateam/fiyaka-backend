4fc651b1f45f0b281700e5336ec120fdacd6b70f
kyoshi <akusgulu309@gmail.com>
2020-12-28 12:16:36 +0300

    Fix merge error

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
 import express, { Application } from 'express';
-import swaggerUI from 'swagger-ui-express';
 require('./db/mongoose');
 import landlordRouter from './routes/landlord';
 import houseRouter from './routes/house';
 import tenantRouter from './routes/tenant';
 import authRouter from './routes/auth';
-//import * as swaggerDocs from './swagger.json';
-//import cors from 'cors';
+import swaggerUI from 'swagger-ui-express';
+import * as swaggerDocs from './swagger.json';
+import cors from 'cors';
 
 const app: Application = express();
 
-//app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
-//app.use(cors());
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+app.use(cors());
 
 app.use(express.json());
 
