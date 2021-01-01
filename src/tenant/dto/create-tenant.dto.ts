e5f21f6c41cdec8bf212affffeab355b6f02421a
Geogear <mustafaenesbilgic@hotmail.com>
2021-01-01 23:10:49 +0300

    Updated tenant dto

--- src/tenant/dto/create-tenant.dto.ts
+++ src/tenant/dto/create-tenant.dto.ts
@@ -1,1 +1,33 @@
-export class CreateTenantDto {}
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateTenantDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  readonly password: string;
+
+  @IsBoolean()
+  readonly timestamps: boolean;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly _landlord: string;
+
+  @IsString()
+  readonly _house: string;
+}
