987797e9a15ffda8665270978ca9d18a15773ddb
Geogear <mustafaenesbilgic@hotmail.com>
2021-01-01 23:47:45 +0300

    Created user dto

---
+++ src/auth/dto/create-user.dto.ts
@@ -0,0 +1,29 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  readonly password: string;
+
+  @IsBoolean()
+  readonly timestamps: boolean;
+
+  @IsString()
+  readonly role: string;
+}
