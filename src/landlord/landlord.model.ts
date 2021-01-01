f85ee606768566264b431cad6a39656766118f3a
Geogear <mustafaenesbilgic@hotmail.com>
2021-01-01 23:46:18 +0300

    Updated landlord model

--- src/landlord/landlord.model.ts
+++ src/landlord/landlord.model.ts
@@ -1,36 +1,14 @@
-import mongoose, { Model } from 'mongoose';
-import { userSchema, IUser } from '../auth/user.model';
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+import { User } from 'src/auth/user.model';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ILandlord extends IUser {
-  houses: string; // placeholder types for now
-  tenants: string; // placeholder types for now
+@Schema()
+export class Landlord extends User {
+  @Prop({ type: [Types.ObjectId], ref: 'Tenant' })
+  _tenants: string;
+
+  @Prop({ type: [Types.ObjectId], ref: 'House' })
+  _houses: string;
 }
 
-export interface ILandlordModel extends Model<ILandlord> {
-  findByCredentials(email: string, password: string): Promise<ILandlord>;
-}
-
-const landlordSchema: mongoose.Schema = new mongoose.Schema();
-landlordSchema.add(userSchema).add({
-  role: { type: String, default: 'Landlord' },
-});
-
-landlordSchema.virtual('houses', {
-  ref: 'House',
-  localField: '_id',
-  foreignField: '_owner',
-});
-
-landlordSchema.virtual('tenants', {
-  ref: 'Tenant',
-  localField: '_id',
-  foreignField: '_landlord',
-});
-
-const Landlord = mongoose.model<ILandlord, ILandlordModel>(
-  'Landlord',
-  landlordSchema
-);
-
-export default Landlord;
+export const LandlordSchema = SchemaFactory.createForClass(Landlord);
