c4ab6606cd53dde8fdf0583809967966938a1432
xinoip <pionix@protonmail.com>
2021-01-01 23:29:00 +0300

    feat/validation: add validation schema to post dto

--- src/proof/dto/proof-post.dto.ts
+++ src/proof/dto/proof-post.dto.ts
@@ -1,9 +1,17 @@
+import { IsInt, IsString } from 'class-validator';
+
 export class ProofPostReq {
+  @IsString()
   title: string;
+
+  @IsString()
   name: string;
+
+  @IsInt()
   luckyNumber: number;
 }
 
 export class ProofPostRes extends ProofPostReq {
+  @IsString()
   id: string;
 }
