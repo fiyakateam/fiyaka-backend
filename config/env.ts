e86aad8e4a7efd83a3313700c3b1812ba33f05c8
kyoshi <akusgulu309@gmail.com>
2021-01-01 17:12:12 +0300

    Migrate commit cont.

---
+++ config/env.ts
@@ -0,0 +1,7 @@
+export default {
+  port: process.env.PORT,
+  mongoURL: process.env.MONGODB_URL,
+  jwtSecret: process.env.JWT_SECRET,
+  sendgridKey: process.env.SENDGRID_API_KEY,
+  emailSource: process.env.EMAIL_SOURCE,
+};
--- tests/landlord.test.ts
+++
@@ -1,3 +0,0 @@
-test('Foo', () => {
-  expect(1).toBe(1);
-});
