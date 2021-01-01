5b42acb0806c90c1a85121fc8cce4bd408dbe18b
kyoshi <akusgulu309@gmail.com>
2021-01-01 22:12:09 +0300

    Setup swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import env from 'config/env';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const options = new DocumentBuilder()
+    .setTitle('Fiyaka API')
+    .setDescription('Fiyaka API Routes')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(env.port || 3000);
 }
 bootstrap();
