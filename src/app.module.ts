ebde45b44f0709c86e2e245ad007145b5cafb667
xinoip <pionix@protonmail.com>
2021-01-01 21:31:24 +0300

    feat/app: remove app service & add landing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { HouseModule } from './house/house.module';
 import { TenantModule } from './tenant/tenant.module';
 import { LandlordModule } from './landlord/landlord.module';
@@ -17,6 +16,6 @@ import env from 'config/env';
     MongooseModule.forRoot(env.mongoURL),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
