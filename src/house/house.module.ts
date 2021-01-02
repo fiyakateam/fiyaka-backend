1b94570e9d5dcc2fe9cb6a3b244740abf89841d2
kyoshi <akusgulu309@gmail.com>
2021-01-02 22:26:15 +0300

    Fix mongoose error in modules

--- src/house/house.module.ts
+++ src/house/house.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { HouseService } from './service/house.service';
 import { HouseController } from './controller/house.controller';
+import { House, HouseSchema } from './model/house.model';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: House.name, schema: HouseSchema }]),
+  ],
   controllers: [HouseController],
   providers: [HouseService],
 })
