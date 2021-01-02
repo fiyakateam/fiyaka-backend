27ab616fee43786b107e9447fb6433c63be509dd
kyoshi <akusgulu309@gmail.com>
2021-01-02 22:26:44 +0300

    Setup jwt for auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,18 @@ import { AuthService } from './service/auth.service';
 import { AuthController } from './controller/auth.controller';
 import { LandlordModule } from 'src/landlord/landlord.module';
 import { TenantModule } from 'src/tenant/tenant.module';
-
+import { JwtModule } from '@nestjs/jwt';
+import env from 'config/env';
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [LandlordModule, TenantModule],
+  imports: [
+    LandlordModule,
+    TenantModule,
+    JwtModule.register({
+      secret: env.jwtSecret,
+      signOptions: { expiresIn: '1y' },
+    }),
+  ],
 })
 export class AuthModule {}
