d130a2a56fe4da19bc5c75e65d02c1a1d84abf3b
Geogear <mustafaenesbilgic@hotmail.com>
2021-01-02 23:03:08 +0300

    Updated module files

--- src/landlord/landlord.module.ts
+++ src/landlord/landlord.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { LandlordService } from './service/landlord.service';
 import { LandlordController } from './controller/landlord.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Landlord, LandlordSchema } from './model/landlord.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Landlord.name, schema: LandlordSchema },
+    ]),
+  ],
   controllers: [LandlordController],
   providers: [LandlordService],
   exports: [LandlordService],
--- src/tenant/tenant.module.ts
+++ src/tenant/tenant.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TenantService } from './service/tenant.service';
 import { TenantController } from './controller/tenant.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Tenant, TenantSchema } from './model/tenant.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Tenant.name, schema: TenantSchema }]),
+  ],
   controllers: [TenantController],
   providers: [TenantService],
   exports: [TenantService],
